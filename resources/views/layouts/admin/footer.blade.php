c6ad25834832983ba2d183a38b380a7b5f5e52d4
faldiansyahkesuma <faldikusuma11@gmail.com>
2024-11-18 09:57:10 +0000

    firstcommit

---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy;
+        <script>
+            var CurrentYear = new Date ().getFullyear()
+            document.write(CurrentYear)
+        </script>
+        , designed & developed by <a href="#" class="text-muted">RPL</a>
+    </div>
+</footer>
---
+++ resources/views/layouts/user/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer-area section_gap">
+    <div class="container">
+        <div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
+            <p class="footer-text m=0">
+                <!-- Link back to Colorlib can't be removed, Template is licensed under CC BY 3.0. -->
+                Copyright &copy;
+                <script>
+                    document.write(new Date().getFullYear());
+                </script> All rights reserved | THis template is made with <i class="fa fa-heart-0"
+                    aria-hidden="true"></i> by <a href="https://colorlib.com" target="-blank">Colorlib</a>
+                <!-- Link back to Colorlib can't be removed, Template is licensed under CC BY 3.0. -->
+            </p>
+        </div>
+    </div>
+</footer>
+
